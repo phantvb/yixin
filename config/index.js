20640d642bf4519bd07b7b9130ed5017acab65e8
zhuwwG <zhuwwSMD666>
2018-08-14 15:01:46 +0800

    commit index.js

--- config/index.js
+++ config/index.js
@@ -45,10 +45,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, 'D:\\idea_workspace\\icc\\icc-parent\\icc-interface\\src\\main\\resources\\static\\view\\index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, 'D:\\idea_workspace\\icc\\icc-parent\\icc-interface\\src\\main\\resources\\static\\view'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
