954fbaa30b39815b399aca3af22d663898e821c2
zhuwwG <zhuwwSMD666>
2018-08-21 11:28:36 +0800

    修改忘记密码问题

--- src/components/resetLogin.vue
+++ src/components/resetLogin.vue
@@ -23,11 +23,11 @@
                         </div>
                         <!-- 新密码 -->
                         <div class="loginForm">
-                           <el-input placeholder="请输入新密码" suffix-icon="el-icon-view" v-model="formPass.newPassWord"></el-input>  
+                           <el-input placeholder="请输入新密码" type="password" suffix-icon="el-icon-view" v-model="formPass.newPassWord"></el-input>
                         </div>
                         <!-- 二次确认密码 -->
                         <div class="loginForm">
-                           <el-input placeholder="请再次输入新密码" suffix-icon="el-icon-view" v-model="formPass.PassWord"></el-input>  
+                           <el-input placeholder="请再次输入新密码" type="password" suffix-icon="el-icon-view" v-model="formPass.PassWord"></el-input>
                         </div>
                         <div class="loginForm">
                             <!-- 验证码输入框 -->
@@ -35,7 +35,7 @@
                                 <el-input placeholder="验证码" v-model="formPass.flag"></el-input>
                             </div>
                             <!-- 验证码图片 -->
-                            <img src="https://10.240.80.72:8443/icc-interface/new/verifyCode" alt="" class="verifyImg">
+                            <img :src="verifyImg" alt="" class="verifyImg" @click="verifyChange">
                         </div>
                         <div class="verifyText">
                            <!-- 此处为验证提示信息 -->
@@ -79,10 +79,27 @@ export default {
             yzInfo:'',
             emilErr:'',
             tokens:'',
+            verifyImg:''
         }
     },
     created(){
-        this.getUrl();
+        //通过点击邮箱返回的链接，地址中拿到token
+        this.tokens = this.$route.query.token;
+        //  回填在重置密码上方
+        let parameter = {
+          "token":this.tokens
+        }
+        this.$ajax.post(this.$preix+'/new/forgetStep2',parameter)
+          .then( (res) => {
+            if(res.data.code==200){
+              this.zhuceEmil = res.data.info
+            }else{
+              this.emilErr = '重置密码链接已失效！请点击返回登陆页面，重新点击忘记密码，重新获取重置密码链接。'
+            }
+          });
+    },
+    mounted(){
+      this.verifyChange();
     },
     methods: {
         msgErrorShow(msg){
@@ -91,34 +108,8 @@ export default {
                 type: "error"
             });
         },
-        // zhuweiwen@yixin.im
-        //获取URL里的token
-        getUrl(){
-            //  this.$route.params.token
-            //通过点击邮箱返回的链接，地址中拿到token
-             console.log(this.$route.params.token);
-             let tokenInfo = window.location.href;
-             console.log(tokenInfo.split('=')[1]);
-             if(tokenInfo){
-                this.tokens = tokenInfo.split('=')[1];
-                //  回填在重置密码上方
-                let parameter = {
-                    "token":this.tokens
-                }
-                this.$ajax.post(this.$preix+'/new/forgetStep2',parameter)
-                    .then( (res) => {
-                        if(res.data.code==200){
-                            console.log(res);
-                            //获取成功
-                            this.zhuceEmil = res.data.info
-                        }else{
-                            // 获取失败
-                            this.msgErrorShow(res.data.message);
-                            this.emilErr = '重置密码链接已失效！请点击返回登陆页面，重新点击忘记密码，重新获取重置密码链接。'
-                        }
-                })
-             }
-             console.log(window.location.href);
+        verifyChange(){
+          this.verifyImg=this.$preix+'/new/verifyCode?ts='+new Date().getTime();
         },
         //从重置密码前往登陆
         goLogins(){
@@ -153,18 +144,16 @@ export default {
                "email":this.zhuceEmil,
                "password":this.formPass.newPassWord,
                "password2":this.formPass.PassWord,
-               "verifyCode":this.formPass.flag
+               "verifyCode":this.formPass.flag,
+               "token" : this.tokens
             }
             this.$ajax.post(this.$preix+'/new/resetPassword',parameter)
                 .then( (res) => {
                     if(res.data.code==200){
-                        console.log(res);
-                        this.msgErrorShow(res.data.message);
+                      this.$message({message: "重置密码成功",type: "success"});
                     }else{
                        // 重置失败
                        this.msgErrorShow(res.data.message);
-                       this.zhuceEmil = '';
-                       this.emilErr = '重置密码链接已失效！请点击返回登陆页面，重新点击忘记密码，重新获取重置密码链接。'
                     }
             })
         },
@@ -233,7 +222,7 @@ export default {
 }
 /* login输入框样式 */
 .loginForm,.loginForms{
-   margin-bottom: 10px; 
+   margin-bottom: 10px;
    height: 40px;
    display: flex;
    align-items: center;
@@ -255,10 +244,10 @@ export default {
 /* 登陆和发送邮件按钮样式 */
 .btnLoginSubmit{
    border: 0;
-   width: 100%; 
+   width: 100%;
    height:40px;
-   font-size: 16px; 
-   color: #fff; 
+   font-size: 16px;
+   color: #fff;
    cursor: pointer;
    background:#84abf8;
    background:-webkit-linear-gradient(left, #84abf8, #84abf8);
