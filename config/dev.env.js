f656f38ff8e2f96127ad3882a04659a4eeefae85
zhuwwG <zhuwwSMD666>
2018-09-21 16:10:06 +0800

    修改开发、线上不同环境接口地址设置成不一样

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  API_ROOT: '"https://10.240.80.72:8443/icc-interface"'
 })
