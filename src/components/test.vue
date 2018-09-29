d5dfe0d726c899623152a2a6f13f52415c3541c8
phantvb <17858803852@163.com>
2018-09-29 12:00:14 +0800

    闪现

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,88 +1,83 @@
 <template>
-    <div>
-      <div contenteditable='true' id="bookTextarea" @input="test">
-      hahaha
-    </div>
-    </div>
+<div>
+  <el-tree :data="navs" :props="defaultProps" node-key="id" ref="tree" highlight-current> </el-tree>
+　　<el-button @click="setCheckedNodes">展开所有菜单</el-button>
+　　<el-button @click="setCheckedKeys">收起所有菜单</el-button>
+</div>
+　　
 </template>
-    
 <script>
-import Aplayer from './component/a_player.vue';
-export default {
-	name: 'test',
-	data() {
-		return {
-      textpos:null,
-      data: [{
+export default { 
+　　data() {
+　　　　return {
+　　　　　　navs:[{
+          id: 1,
           label: '一级 1',
           children: [{
+            id: 4,
             label: '二级 1-1',
             children: [{
+              id: 9,
               label: '三级 1-1-1'
+            }, {
+              id: 10,
+              label: '三级 1-1-2'
             }]
           }]
         }, {
+          id: 2,
           label: '一级 2',
           children: [{
-            label: '二级 2-1',
-            children: [{
-              label: '三级 2-1-1'
-            }]
+            id: 5,
+            label: '二级 2-1'
           }, {
-            label: '二级 2-2',
-            children: [{
-              label: '三级 2-2-1'
-            }]
+            id: 6,
+            label: '二级 2-2'
           }]
         }, {
+          id: 3,
           label: '一级 3',
-        children: [{
-            label: '二级 3-1',
-            children: [{
-              label: '三级 3-1-1'
-            }]
+          children: [{
+            id: 7,
+            label: '二级 3-1'
           }, {
+            id: 8,
             label: '二级 3-2',
             children: [{
+             id: 11,
               label: '三级 3-2-1'
+            }, {
+              id: 12,
+              label: '三级 3-2-2'
+            }, {
+              id: 13,
+              label: '三级 3-2-3'
             }]
           }]
-        }],
-        defaultProps: {
+        }],   //所有菜单 
+　　　　　　 defaultProps: {
+          id:'id',
           children: 'children',
           label: 'label'
-        }
-    };
-	},
-	components: { Aplayer },
-	methods: {
-    test(e){
-      let oTextarea = document.getElementById('bookTextarea');
-      oTextarea.focus();  
-      if (e.data=='@') {
-        this.textpos=window.getSelection().getRangeAt(0)||window.getSelection().getRangeAt(0)
-        oTextarea.innerHTML=oTextarea.innerHTML.replace('@','<div class="outnote" ref="pos">!!</div>');
-        console.log(document.getElementsByClassName('outnote')[0].offsetLeft)
-      }
-    },
-    insertBook(bookMessage){
-      var tc = document.getElementById("bookTextarea");  
-    }
-  },
-};
+        },
+ 
+　　　　　　defaultExpand:true,  //默认展开所有节点
+　　　　}
+　　},
+　　methods: {
+　　　　setCheckedKeys() {　　
+　　　　　　this.defaultExpand=false;
+            console.log(this.$refs.tree,this.$refs.tree.store._getAllNodes())
+　　　　　　for(var i=0;i<this.$refs.tree.store._getAllNodes().length;i++){
+　　　　　　　　this.$refs.tree.store._getAllNodes()[i].expanded=false;
+　　　　　　}
+　　　　},
+　　　　setCheckedNodes() {
+　　　　　　this.defaultExpand=true; //展开所有节点
+　　　　　　for(var i=0;i<this.$refs.tree.store._getAllNodes().length;i++){
+　　　　　　　　this.$refs.tree.store._getAllNodes()[i].expanded=this.defaultExpand;
+　　　　　　}
+　　　　}
+　　}
+}
 </script>
-<style scopp scoped>
-.outnote{
-  position: relative;
-  display: inline;
-}
-div#bookTextarea{
-  text-align: left;
-  border:1px solid #ccc;
-  width: 500px;
-  height: 300px;
-}
-textarea{
-  float: left;
-}
-</style>
